bf0ce4bbb822f193d2c37f14311571a3daa0ae1d
Durga Prasad Donga <dp21422@corp.ojas-it.com>
2022-11-03 16:32:02 +0530

    java mvc

---
+++ src/main/java/com/example/demo/SpringMvcThymleafCrud11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymleafCrud11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcThymleafCrud11Application.class, args);
+	}
+
+}
